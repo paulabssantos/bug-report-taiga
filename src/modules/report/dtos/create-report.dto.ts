0ab93bd7c7b7c2f971194ebe3284f1c9b6375495
paulabssantos <paulablsantos7@gmail.com>
2023-06-07 16:41:13 -0400

    feat: adiciona matricula e nome na criacao de report

--- src/modules/report/dtos/create-report.dto.ts
+++ src/modules/report/dtos/create-report.dto.ts
@@ -1,15 +1,23 @@
-import { IsEmail, IsEmpty, IsString } from "class-validator"
+import { IsEmail, IsInt, IsNotEmpty, IsString } from "class-validator"
 
 export class CreateReportDTO{
-    @IsEmpty({message: 'Email não pode ficar vazio'})
+    @IsNotEmpty({message: 'Nome não pode ficar vazio'})
+    @IsString({message: 'Nome precisa uma string'})
+    name: string
+
+    @IsNotEmpty({message: 'Matricula não pode ficar vazio'})
+    @IsInt({message: 'Matricula precisa um número'})
+    registration: number
+
+    @IsNotEmpty({message: 'Email não pode ficar vazio'})
     @IsEmail()
     email: string
 
-    @IsEmpty({message: 'Descrição não pode ficar vazio'})
+    @IsNotEmpty({message: 'Descrição não pode ficar vazio'})
     @IsString({message: 'Descrição precisa uma string'})
     description: string
 
-    @IsEmpty({message: 'Sistema não pode ficar vazio'})
+    @IsNotEmpty({message: 'Sistema não pode ficar vazio'})
     @IsString({message: 'Sistema precisa uma string'})
     system: string
 }
