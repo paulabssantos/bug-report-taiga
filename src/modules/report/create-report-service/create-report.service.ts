c5c5f344e5d8797369db66c1b317f04b17d7f79e
paulabssantos <paulablsantos7@gmail.com>
2023-06-15 14:04:45 -0400

    feat: cria report no taiga

--- src/modules/report/create-report-service/create-report.service.ts
+++ src/modules/report/create-report-service/create-report.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateReportDTO } from '../dtos/create-report.dto';
+import { CreateUserStoryDTO, createUserStory } from 'src/shared/http/api/api-taiga';
 
 @Injectable()
 export class CreateReportService {
+    constructor(){}
     async execute(createReportDto: CreateReportDTO){
-        console.log("Cria report")
+        const userStory: CreateUserStoryDTO = {
+            description: `Nome: ${createReportDto.name}\nMatrícula: ${createReportDto.registration}\nDescrição do problema: ${createReportDto.description}\n`,
+            project: 808519,
+            subject: createReportDto.title,
+            tags: [
+                createReportDto.system,
+            ]
+        }
+        await createUserStory(userStory).then((res)=>{
+            console.log(res)
+        })
     }
 }
