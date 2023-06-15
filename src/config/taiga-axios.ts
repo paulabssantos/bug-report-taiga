560623bae8353eaf2d703ea0586029da41ea9906
paulabssantos <paulablsantos7@gmail.com>
2023-06-15 15:50:59 -0400

    fix: corrige acesso a variavei de ambiente com importacao de dotenv/config

--- src/config/taiga-axios.ts
+++ src/config/taiga-axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import 'dotenv/config'
 
 const taiga_instance = axios.create({ baseURL: "https://api.taiga.io/api/v1/", headers: {
     'Authorization': `Bearer ${process.env.TAIGA_TOKEN}`
-}})
+ }})
 
 export { taiga_instance }
