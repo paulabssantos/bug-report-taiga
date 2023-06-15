56c6d8f68ffcca114398f7fba03f4654ec4e57bb
paulabssantos <paulablsantos7@gmail.com>
2023-06-15 14:03:27 -0400

    config: configura condigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReportModule } from './modules/report/report.module';
+import { DatabaseModule } from './shared/infra/database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ReportModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }), DatabaseModule, ReportModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
