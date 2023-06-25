5c8bce67981363f90af65307769e40446664c72d
paulabssantos <paulablsantos7@gmail.com>
2023-06-25 08:41:16 -0400

    fix: coloca token de refresh em cookie

--- src/shared/http/middlewares/taiga-login.middleware.ts
+++ src/shared/http/middlewares/taiga-login.middleware.ts
@@ -5,13 +5,15 @@ import { login, refreshToken } from '../api/api-taiga';
 @Injectable()
 export class TaigaLoginMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
-      await refreshToken(req.cookies['taiga-refresh']).then((data)=>{
-        res.cookie('taiga-token',data.auth_token)
-      }).catch(async ()=>{
-        const login_response = await login()
-        res.cookie('taiga-refresh',login_response.refresh)
-        res.cookie('taiga-token',login_response.auth_token)
-      })
-      next();
+    if (req.cookies['taiga-refresh'] && req.cookies['taiga-refresh'] != 'undefined') {
+      const refresh_response = await refreshToken(req.cookies['taiga-refresh'])
+      res.cookie('taiga-refresh',refresh_response.refresh)
+      res.cookie('taiga-token', refresh_response.auth_token)
+    } else {
+      const login_response = await login()
+      res.cookie('taiga-refresh', login_response.refresh)
+      res.cookie('taiga-token', login_response.auth_token)
     }
+    next();
+  }
 }
