42fb6e158ff69ac1838d69a8e32cc176f5932f03
paulabssantos <paulablsantos7@gmail.com>
2023-07-03 16:06:32 -0400

    fix: adiciona tempo de expiracao de cookie no corpo da requisicao

--- src/shared/http/middlewares/taiga-login.middleware.ts
+++ src/shared/http/middlewares/taiga-login.middleware.ts
@@ -1,20 +1,28 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { login, refreshToken } from '../api/api-taiga';
+import { TaigaException } from 'src/shared/errors/taiga-exception';
+import * as dayjs from 'dayjs'
 
 @Injectable()
 export class TaigaLoginMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
-    if (req.cookies['taiga-refresh'] && req.cookies['taiga-refresh'] != 'undefined') {
-      const refresh_response = await refreshToken(req.cookies['taiga-refresh'])
-      res.cookie('taiga-refresh',refresh_response.refresh)
-      res.cookie('taiga-token',refresh_response.auth_token)
+    if (!req.cookies['taiga-token-expiration'] || dayjs(dayjs().format("YYYY-MM-DDTHH:mm:ss-00:00")).diff(req.cookies['taiga-token-expiration'], 'minutes', true) >= 4) {
+      await refreshToken(req.cookies['taiga-refresh']).then((refresh_response) => {
+        res.cookie('taiga-refresh', refresh_response.refresh)
+        res.cookie('taiga-token', refresh_response.auth_token)
+        res.cookie('taiga-token-expiration',dayjs( dayjs().format("YYYY-MM-DDTHH:mm:ss-00:00")).add(4,'minute'))
+      }).catch(async (err: Error | TaigaException) => {
+        await login().then((login_response) => {
+          res.cookie('taiga-refresh', login_response.refresh)
+          res.cookie('taiga-token', login_response.auth_token)
+          res.cookie('taiga-token-expiration',dayjs( dayjs().format("YYYY-MM-DDTHH:mm:ss-00:00")).add(4,'minute'))
+        })
+      }).finally(() => {
+        next();
+      })
+    } else {
+      next();
     }
-     else {
-      const login_response = await login()
-      res.cookie('taiga-refresh',login_response.refresh)
-      res.cookie('taiga-token',login_response.auth_token)
-    }
-    next();
   }
 }
