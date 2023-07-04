997f4a812b455618bc7f25af9d35600c1b1c7427
paulabssantos <paulablsantos7@gmail.com>
2023-07-04 08:29:26 -0400

    fix: corrige lógica de validação de cookie

--- src/shared/http/middlewares/taiga-login.middleware.ts
+++ src/shared/http/middlewares/taiga-login.middleware.ts
@@ -7,18 +7,18 @@ import * as dayjs from 'dayjs'
 @Injectable()
 export class TaigaLoginMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
-    if (!req.cookies['taiga-token-expiration'] || dayjs(dayjs().format("YYYY-MM-DDTHH:mm:ss-00:00")).diff(req.cookies['taiga-token-expiration'], 'minutes', true) >= 4) {
+    if (!req.cookies['taiga-token-expiration']) {
+      await login().then((login_response) => {
+        res.cookie('taiga-refresh', login_response.refresh)
+        res.cookie('taiga-token', login_response.auth_token)
+        res.cookie('taiga-token-expiration', dayjs(dayjs().format("YYYY-MM-DDTHH:mm:ss-00:00")).add(4, 'minute'))
+      })
+      next();
+    } else if (dayjs(dayjs().format("YYYY-MM-DDTHH:mm:ss-00:00")).diff(req.cookies['taiga-token-expiration'], 'minutes', true) >= 4) {
       await refreshToken(req.cookies['taiga-refresh']).then((refresh_response) => {
         res.cookie('taiga-refresh', refresh_response.refresh)
         res.cookie('taiga-token', refresh_response.auth_token)
-        res.cookie('taiga-token-expiration',dayjs( dayjs().format("YYYY-MM-DDTHH:mm:ss-00:00")).add(4,'minute'))
-      }).catch(async (err: Error | TaigaException) => {
-        await login().then((login_response) => {
-          res.cookie('taiga-refresh', login_response.refresh)
-          res.cookie('taiga-token', login_response.auth_token)
-          res.cookie('taiga-token-expiration',dayjs( dayjs().format("YYYY-MM-DDTHH:mm:ss-00:00")).add(4,'minute'))
-        })
-      }).finally(() => {
+        res.cookie('taiga-token-expiration', dayjs(dayjs().format("YYYY-MM-DDTHH:mm:ss-00:00")).add(4, 'minute'))
         next();
       })
     } else {
