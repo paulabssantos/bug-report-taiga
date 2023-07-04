b4bd181844f230971c0eb04e41d2e7477c2be8a8
paulabssantos <paulablsantos7@gmail.com>
2023-07-04 08:30:27 -0400

    fix: muda metodo http para get

--- src/modules/report/list-report-service/list-report.controller.ts
+++ src/modules/report/list-report-service/list-report.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Param, Post, Query, Request } from '@nestjs/common';
+import { Controller, Get, Param, Request } from '@nestjs/common';
 import { ListReportService } from './list-report.service';
 
 @Controller('report')
 export class ListReportController {
     constructor(private listReportService: ListReportService) { }
 
-    @Post('/:email')
+    @Get('/:email')
     async create(@Request() req, @Param('email') email: string) {
         const token = req.cookies['taiga-token']
         return await this.listReportService.execute(email, token)
