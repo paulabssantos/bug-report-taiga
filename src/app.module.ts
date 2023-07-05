c31ad49a0646abae760d409ef9713abd3f0b7eea
paulabssantos <paulablsantos7@gmail.com>
2023-07-05 12:36:44 -0400

    feat: adiciona server static para visualizacao de imagens

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,22 @@ import { ReportModule } from './modules/report/report.module';
 import { DatabaseModule } from './shared/infra/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaigaLoginMiddleware } from './shared/http/middlewares/taiga-login.middleware';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }), DatabaseModule, ReportModule],
+  }),
+  ServeStaticModule.forRoot({
+    rootPath: './uploads',
+    serveRoot: '/uploads'
+  }),
+    DatabaseModule, ReportModule],
   controllers: [],
   providers: [],
 })
-export class AppModule implements NestModule{
-  configure(consumer: MiddlewareConsumer){
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
     consumer.apply(TaigaLoginMiddleware).forRoutes('report')
   }
 }
