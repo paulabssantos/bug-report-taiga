13d633d17bf51a5ad9d385ca8c1bed752d630452
paulabssantos <paulablsantos7@gmail.com>
2023-07-06 11:09:06 -0400

    feat: restringe tamanho e tipo de arquivo a ser enviado

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,6 +1,18 @@
 import * as multer from "multer"
 import * as crypto from 'crypto'
+import { HttpException, HttpStatus } from "@nestjs/common"
 export const multerConfig = {
+    fileFilter(req,file,callback){
+        if (file.mimetype.startsWith('image/')) {
+            callback(null, true)
+        }
+        else {
+            callback(new HttpException('Formato de arquivo precisa ser uma imagem', HttpStatus.BAD_REQUEST), false)
+        }
+    },
+    limits: {
+        fileSize: 1024 * 1024 * 2, 
+      },
     dest: './uploads',
     storage: multer.diskStorage({
         destination(req, file, callback) {
