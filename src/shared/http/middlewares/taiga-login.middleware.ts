3e8e08311064e0c02215baff840bfd53d78a311a
paulabssantos <paulablsantos7@gmail.com>
2023-07-06 09:27:06 -0400

    fix: chama rota de login caso cookie nao exista e aumenta tempo de verificacao

--- src/shared/http/middlewares/taiga-login.middleware.ts
+++ src/shared/http/middlewares/taiga-login.middleware.ts
@@ -6,7 +6,18 @@ import * as dayjs from 'dayjs'
 @Injectable()
 export class TaigaLoginMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
-    if (!req.cookies['taiga-token-expiration'] || dayjs(dayjs().format("YYYY-MM-DDTHH:mm:ss-00:00")).diff(req.cookies['taiga-token-expiration'], 'minutes', true) >= 4) {
+    if (!req.cookies['taiga-token-expiration']) {
+      try {
+        const login_response = await login()
+        res.cookie('taiga-refresh', login_response.refresh)
+        res.cookie('taiga-token', login_response.auth_token)
+        res.cookie('taiga-token-expiration', dayjs(dayjs().format("YYYY-MM-DDTHH:mm:ss-00:00")).add(10, 'minute'))
+      } catch (error) {
+      } finally {
+        next()
+      }
+    }
+    else if (dayjs(dayjs().format("YYYY-MM-DDTHH:mm:ss-00:00")).diff(req.cookies['taiga-token-expiration'], 'minutes', true) >= 10) {
       try {
         const refresh_response = await refreshToken(req.cookies['taiga-refresh'])
         res.cookie('taiga-refresh', refresh_response.refresh)
