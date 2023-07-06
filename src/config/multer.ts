9b36467f3ffc6e4ba5f5993522c43d1226a0092c
paulabssantos <paulablsantos7@gmail.com>
2023-07-06 14:54:08 -0400

    fix: corrige logica de validacao de imagem'

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -3,20 +3,20 @@ import * as crypto from 'crypto'
 import { HttpException, HttpStatus } from "@nestjs/common"
 export const multerConfig = {
     limits: {
-        fileSize: 1024 * 1024 * 2, 
-      },
-    fileFilter(req,file,callback){
+        fileSize: 1024 * 1024 * 2,
+    },
+    fileFilter(req, file, callback) {
         if (file.mimetype.startsWith('image/')) {
-            callback(null, true)
+            if (file.size > 1024 * 1024 * 2) {
+                callback(new HttpException('Imagem deve ter no máximo 2MB', HttpStatus.BAD_REQUEST), false)
+            } else {
+                callback(null, true)
+            }
         }
         else {
             callback(new HttpException('Formato de arquivo precisa ser uma imagem', HttpStatus.BAD_REQUEST), false)
         }
-        if(file.size > 1024 * 1024 * 2){
-            callback(new HttpException('Imagem deve ter no máximo 2MB', HttpStatus.BAD_REQUEST), false)
-        }else{
-            callback(null, true)
-        }
+
     },
     dest: './uploads',
     storage: multer.diskStorage({
